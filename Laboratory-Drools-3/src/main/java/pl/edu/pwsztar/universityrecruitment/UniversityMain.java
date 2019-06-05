4e2310de7c6dd9ef6bd6f4c8e8e59cb1cdc1d2db
tpotempa <tpotempa@gmail.com>
2019-06-05 13:38:26 +0200

    Base version.

---
+++ Laboratory-Drools-3/src/main/java/pl/edu/pwsztar/universityrecruitment/UniversityMain.java
@@ -0,0 +1,19 @@
+package pl.edu.pwsztar.universityrecruitment;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class UniversityMain {
+
+	public static void main(String[] args) {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+		KieSession kSession = kContainer.newKieSession("ksession-rules");
+		
+		UniversityCandidate uc = new UniversityCandidate(1L, "Anna", "Kowalska", 120, Boolean.FALSE, "Ekonomia", Boolean.FALSE);
+		kSession.insert(uc);		
+		kSession.fireAllRules();
+	}
+
+}
