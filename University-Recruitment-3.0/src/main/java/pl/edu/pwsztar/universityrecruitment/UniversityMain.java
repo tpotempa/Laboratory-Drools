3869694eff1bdb209b68b90b70a368d282b1e2be
tpotempa <tpotempa@gmail.com>
2020-06-12 12:22:25 +0200

    Preparing for a rename.

--- University-Recruitment-3.0/src/main/java/pl/edu/pwsztar/universityrecruitment/UniversityMain.java
+++ University-Recruitment-3.0/src/main/java/pl/edu/pwsztar/universityrecruitment/UniversityMain.java
@@ -16,7 +16,7 @@ public class UniversityMain {
 		KieSession kSession = kContainer.newKieSession("ksession-rules");
 		
 		UniversityCandidate uc1 = new UniversityCandidate(1L, "Anna", "Kowalska", 120.0, Boolean.FALSE, "Informatyka", Boolean.TRUE, "Female");
-		UniversityCandidate uc2 = new UniversityCandidate(2L, "Jacek", "Nowak", 80.0, Boolean.FALSE, "Informatyka", Boolean.FALSE, "Male");		
+		UniversityCandidate uc2 = new UniversityCandidate(2L, "Jacek", "Nowak", 110.0, Boolean.FALSE, "Informatyka", Boolean.FALSE, "Male");		
 		UniversityCandidate uc3 = new UniversityCandidate(3L, "Ewa", "Wiśniowa", 35.0, Boolean.FALSE, "Elektrotechnika", Boolean.FALSE, "Female");
 		UniversityCandidate uc4 = new UniversityCandidate(4L, "Karol", "Gruszka", 135.0, Boolean.FALSE, "Automatyka i robotyka", Boolean.TRUE, "Male");
 		UniversityCandidate uc5 = new UniversityCandidate(5L, "Kinga", "Poziomka", 30.0, Boolean.FALSE, "Elektrotechnika", Boolean.TRUE, "Female");
@@ -37,7 +37,7 @@ public class UniversityMain {
 		// Example 1.2
 		// OPIS: Uruchamianie 1 zbioru składającego się z 3 wykluczjących się reguł kwalifikacyjnych z MODFIKACJĄ faktu powodującą uruchomienie ponownego wnioskowania. 
 		// PYTANIE: W jakim celu używany jest counter?
-		// kSession.getAgenda().getAgendaGroup("one_set_of_rules_modify").setFocus();
+		//kSession.getAgenda().getAgendaGroup("one_set_of_rules_modify").setFocus();
 
 		// Example 1.3
 		// OPIS: Uruchamianie 1 zbioru składającego się z 3 wykluczjących się reguł kwalifikacyjnych z MODFIKACJĄ faktu powodującą uruchomienie ponownego wnioskowania
@@ -65,7 +65,7 @@ public class UniversityMain {
 		// z wykorzystaniem parametru activation-group, który określa, że gdy dowolna reguła należąca do activation-group 
 		// zostanie aktywowana, uruchomienie wszystkich pozostałych należących do grupy jest anulowane 
 		// PYTANIE: Dlaczego nie wszystie fakty tj. kandydaci mają określoną decyzję?
-		// kSession.getAgenda().getAgendaGroup("two_sets_of_rules_salience_activation-group").setFocus();
+		//kSession.getAgenda().getAgendaGroup("two_sets_of_rules_salience_activation-group").setFocus();
 		 
 		System.out.println("Number of facts in Working Memory (Entry Point): " + kSession.getFactCount());
         kSession.addEventListener(new DebugAgendaEventListener());
@@ -82,7 +82,7 @@ public class UniversityMain {
 		*/
 		
 		
-		/*
+		
 		// Examples :: SET 2
 		// Przetwarzanie pojedynczego faktu (Zestaw reguł prostych)
 		kSession.insert(uc1);
@@ -92,7 +92,7 @@ public class UniversityMain {
 		// Uruchamianie tylko jednej reguły kwalifikacyjnej, gdyż nie jest zasadne 2-krotna kwalifikacja kandydata,
 		// z wykorzystaniem parametru activation-group, który określa, że gdy dowolna reguła należąca do activation-group 
 		// zostanie aktywowana, uruchomienie wszystkich pozostałych należących do grupy jest anulowane.
-		// kSession.getAgenda().getAgendaGroup("two_sets_of_rules_salience_activation-group").setFocus();
+		kSession.getAgenda().getAgendaGroup("two_sets_of_rules_salience_activation-group").setFocus();
 
 		// Example 2.2
 		// OPIS: Uruchamianie 2 zbiorów reguł kwalifikacyjnych. Oba zbiory reguł nie wykluczają się tj. mogą być uruchomione reguły z obu zbiorów.
@@ -113,7 +113,7 @@ public class UniversityMain {
 		// Reguły z obu zbiorow "ExamQualification" oraz "OlympicQualification" są wykonywane z MODFIKACJĄ faktu powodującą uruchomienie ponownego wnioskowania.
 		// Reguły posiadają parametry activation-group, salience oraz lock-on-active.
 		// PYTANIE: Jak działa parametr lock-on-active?
-		kSession.getAgenda().getAgendaGroup("two_sets_of_rules_salience_activation-group_modify_lock-on-active").setFocus();
+		// kSession.getAgenda().getAgendaGroup("two_sets_of_rules_salience_activation-group_modify_lock-on-active").setFocus();
 
 		System.out.println("Number of facts in Working Memory (Entry Point): " + kSession.getFactCount());
         kSession.addEventListener(new DebugAgendaEventListener());
@@ -123,7 +123,7 @@ public class UniversityMain {
 				
 		System.out.println(uc1.getCandidateInformation());
 		// End Examples :: SET 2		 
-		*/
+		
 		
 		
 		/*
